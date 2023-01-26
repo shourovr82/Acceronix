19cb3696cf2582430d2805b5122de68bdcddedee
shourovr82 <shafinur512@gmail.com>
2023-01-26 22:49:15 +0600

    get form data from user

--- src/Contexts/Context/AcceronixProvider.js
+++ src/Contexts/Context/AcceronixProvider.js
@@ -1,13 +1,69 @@
-import React, { createContext } from 'react';
-import { getAuth } from 'firebase/auth'
+import React, { createContext, useEffect, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from 'firebase/auth'
 import app from '../FirebaseConfig/firebase.config';
 
 export const AcceronixContext = createContext();
 
 const AcceronixProvider = ({ children }) => {
   const auth = getAuth(app);
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const googleProvider = new GoogleAuthProvider();
+
+
+  const registerAccount = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password)
+  }
+
+  const updateCurrentUserProfile = (userDetails) => {
+    setLoading(true);
+    return updateProfile(auth.currentUser, {
+      displayName: userDetails.displayName,
+      photoURL: userDetails.photoURL
+    })
+  }
+
+  const logInWithEmail = (email, password) => {
+    setLoading(true)
+    return signInWithEmailAndPassword(auth, email, password)
+  }
+
+
+
+  const handleLogout = () => {
+    setLoading(true);
+    return signOut(auth)
+  }
+
+
+
+
+
+  const handleGoogleLogin = () => {
+    setLoading(true)
+    return signInWithPopup(auth, googleProvider)
+  }
+
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, currentUser => {
+      setUser(currentUser);
+      setLoading(false);
+    })
+
+    return () => unsubscribe();
+  }, [])
+
   const contextInfo = {
-    name: 'shafin'
+    handleGoogleLogin,
+    user,
+    loading,
+    handleLogout,
+    registerAccount,
+    updateCurrentUserProfile,
+    logInWithEmail,
   }
   return (
     <div>
--- src/Pages/Authentication/Register/RegisterMain.js
+++ src/Pages/Authentication/Register/RegisterMain.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { HiEye, HiOutlineArrowNarrowRight } from 'react-icons/hi';
 import { GoEyeClosed } from 'react-icons/go';
 import { FaFacebookF } from 'react-icons/fa';
@@ -7,19 +7,89 @@ import './RegistrationMain.css'
 import RegistrationText from './RegistrationText';
 import { useForm } from 'react-hook-form';
 import ReactFlagsSelect from 'react-flags-select';
+import { AcceronixContext } from '../../../Contexts/Context/AcceronixProvider';
 
 
 const RegisterMain = () => {
-  const [showPassword, setShowPassword] = useState(false)
-  const { register, handleSubmit, reset, formState: { errors } } = useForm();
+  const [showPassword, setShowPassword] = useState(false);
   const [selected, setSelected] = useState("");
+  const [toggle, setToggle] = useState(true);
+  const [photoLink, setPhotoLink] = useState('')
+  const [accountType, setAccountType] = useState('Buyer');
+  const { register, handleSubmit, reset, formState: { errors } } = useForm();
   const onSelect = (code) => setSelected(code);
+  const { handleGoogleLogin, registerAccount } = useContext(AcceronixContext);
   const searchable = Boolean("Searchable", false);
+  const imgbbSecretKey = process.env.REACT_APP_imgbb_key;
 
 
-  const handleRegister = (data) => {
-    console.log(data);
+  const getAccountType = () => {
+    if (toggle) {
+      setAccountType('Seller')
+    } else {
+      setAccountType('Buyer')
+    }
   }
+
+  const hostProfilePhoto = (image, formData) => {
+    const url = `https://api.imgbb.com/1/upload?key=${imgbbSecretKey}`;
+    if (image) {
+      fetch(url, {
+        method: 'POST',
+        body: formData
+      })
+        .then(res => res.json())
+        .then(imgData => {
+          const photoURL = imgData?.data?.url;
+          setPhotoLink(photoURL)
+        })
+    }
+  }
+
+
+
+  // register new account
+  const handleRegister = (data) => {
+
+    const image = data.image[0];
+    // host image 
+    if (image) {
+      const formData = new FormData();
+      formData.append(image, formData);
+      if (formData) {
+        hostProfilePhoto(image, formData);
+      }
+      console.log(formData);
+    }
+    const allData = {
+      ...data,
+      accountType,
+      photoURL: photoLink
+    }
+
+    console.log(allData);
+  }
+
+
+
+
+
+
+
+
+
+  // login with google
+  const googleLoginHandle = () => {
+    handleGoogleLogin()
+      .then(result => {
+        const user = result.user;
+      })
+      .catch(e => console.log(e))
+  }
+
+
+
+
 
 
   return (
@@ -28,8 +98,7 @@ const RegisterMain = () => {
       <RegistrationText />
       <form
         onSubmit={handleSubmit(handleRegister)}
-        className='flex  flex-col gap-5'>
-
+        className='flex  flex-col gap-4'>
         {/* Name */}
 
         <div className='flex items-center gap-4'>
@@ -40,9 +109,9 @@ const RegisterMain = () => {
               required: 'Your First name is Required ', minLength: { value: 3, message: 'Your first name must be at least 3' }
             })} />
             <div className="cut bg-base-100"></div>
-            <label for="firstname" className="placeholder flex items-center gap-3">First name  </label>
+            <label htmlFor="firstname" className="placeholder flex items-center gap-3">First name  </label>
             {
-              errors.firstname && <p className='text-white mt-1 text-center border bg-red-700 px-3 rounded-lg'>{errors.firstname?.message}</p>
+              errors.firstname && <p className='text-white mt-1 text-center border bg-red-700 px-3 text-sm rounded-lg'>{errors.firstname?.message}</p>
             }
 
           </div>
@@ -55,9 +124,9 @@ const RegisterMain = () => {
               })}
             />
             <div className="cut"></div>
-            <label for="lastname" className="placeholder">Last name</label>
+            <label htmlFor="lastname" className="placeholder">Last name</label>
             {
-              errors.lastname && <p className='text-white mt-1 text-center border bg-red-700 px-3 rounded-lg'>{errors.lastname?.message}</p>
+              errors.lastname && <p className='text-white mt-1 text-center border bg-red-700 px-3 text-sm rounded-lg'>{errors.lastname?.message}</p>
             }
           </div>
         </div>
@@ -73,8 +142,9 @@ const RegisterMain = () => {
           })} />
           <div className="cut cut-short"></div>
           <label htmlFor="email" className="placeholder">Email</label>
+
           {
-            errors.email && <p className='text-white mt-1 text-center border bg-red-700 px-3 rounded-lg'>{errors.email?.message}</p>
+            errors.email && <p className='text-white text-sm mt-1 text-center border bg-red-700 px-3  rounded-lg'>{errors.email?.message}</p>
           }
         </div>
         {/* Country */}
@@ -91,9 +161,10 @@ const RegisterMain = () => {
 
 
         {/* photo upload */}
-        <div className="input-container  my-2   ">
-          <input type="file" className="file-input focus:outline-none focus:ring-2 ring-[#1f2aa1] ring-offset-0   forminputupload file-input-info rounded-xl
-               w-full"    />
+        <div className="input-container flex items-center forminputuploadFile justify-between  w-full my-2 rounded-xl   ">
+          <input type="file" id='file' className="file-input  focus:outline-none focus:ring-2 ring-[#1f2aa1] ring-offset-0   forminputupload file-input-info rounded-xl w-full"   {...register("image")} />
+          <label htmlFor="file" className="pr-2 text-xs">Upload Your Profile</label>
+
         </div>
 
         {/* Password */}
@@ -107,18 +178,19 @@ const RegisterMain = () => {
 
             />
             <button
+              onClick={() => setShowPassword(!showPassword)}
               type="button" className="absolute right-2 bg-transparent flex items-center justify-center text-gray-700">
               <span
-                onClick={() => setShowPassword(!showPassword)}
+
               >{showPassword ? <HiEye className='text-xl' /> : <GoEyeClosed className='text-xl' />}</span>
 
 
             </button>
             <div className="cut bg-base-100"></div>
-            <label for="firstname" className="placeholder flex items-center gap-3">Password  </label>
+            <label htmlFor="firstname" className="placeholder flex items-center gap-3">Password  </label>
           </div>
           {
-            errors.password && <p className='text-white mt-1 text-center border bg-red-700 px-3 rounded-lg'>{errors.password?.message}</p>
+            errors.password && <p className='text-white text-sm mt-1 text-center border bg-red-700 px-3 rounded-lg'>{errors.password?.message}</p>
           }
         </div>
 
@@ -128,14 +200,28 @@ const RegisterMain = () => {
           <h4 className='text-sm font-bold'>
             Account Type?
           </h4>
-          <p className="btn-switch">
-            <input type="radio" id="yes" name="switch" className="btn-switch__radio btn-switch__radio_yes" />
-            <input type="radio" checked id="no" name="switch" className="btn-switch__radio btn-switch__radio_no" />
-            <label for="yes" className="btn-switch__label btn-switch__label_yes">
-              <span className="btn-switch__txt">Seller</span>
-            </label>
-            <label for="no" className="btn-switch__label btn-switch__label_no"><span className="btn-switch__txt"> Buyer </span></label>
-          </p>
+          <>
+            <div className="flex flex-col justify-center  items-center ">
+              {/*   Switch Container */}
+
+              <div
+                className="md:w-40 md:h-10 w-12 h-6 flex items-center bg-[#0915364f] rounded-full relative p-1 cursor-pointer"
+                onClick={() => {
+                  setToggle(!toggle);
+                  getAccountType()
+                }}
+              >
+                <span className={`absolute transition-transform font-semibold text-black ${toggle ? 'right-5 ' : 'left-5'}`}>{toggle ? 'Buyer' : 'Seller'}</span>
+                {/* Switch */}
+                <div
+                  className={
+                    "bg-black md:w-7 ml-1.5 md:h-7 h-5 w-5 rounded-full shadow-md transform duration-300 ease-in-out" +
+                    (toggle ? null : 'transform translate-x-28')
+                  }
+                ></div>
+              </div>
+            </div>
+          </>
         </div>
 
 
@@ -150,13 +236,15 @@ const RegisterMain = () => {
             <p className='text-slate-400'>Or</p>
           </div>
           <div className='flex items-center justify-between gap-2'>
-            <button className='border rounded-full p-2.5'>
+            <p
+              onClick={() => googleLoginHandle()}
+              className='border  rounded-full p-2.5'>
               <FcGoogle className='text-2xl' />
-            </button>
+            </p>
 
-            <button className='border rounded-full p-2.5'>
+            <p className='border rounded-full p-2.5'>
               <FaFacebookF className='text-2xl text-[#1877f2]' />
-            </button>
+            </p>
           </div>
 
         </div>
